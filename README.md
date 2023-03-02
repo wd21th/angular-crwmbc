bc4ea04d81ac0f292229b4d516285067a92a42f4
wd21th <67710731+wd21th@users.noreply.github.com>
2023-03-02 15:48:32 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-crwmbc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-crwmbc)
---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MaterialExampleModule} from '../material.module';
+import {CdkDragDropHorizontalSortingExample} from './cdk-drag-drop-horizontal-sorting-example';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatNativeDateModule} from '@angular/material/core';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [CdkDragDropHorizontalSortingExample],
+  imports: [
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MatNativeDateModule,
+    MaterialExampleModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [CdkDragDropHorizontalSortingExample],
+})
+export class AppModule {}
